24d4d175baaaaaeeb5e58f6735d19a2352d8598b
ember arlynx <ember@lunar.town>
2025-12-15 18:30:10 -0500

    upd8 slug

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/green-factory/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/mgd-green-factory/' : '/',
 })
